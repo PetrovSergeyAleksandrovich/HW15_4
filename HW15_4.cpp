ea4e51dd585b2add4b4a75a69f0881318bc9787e
PetrovSergeyAleksandrovich <ps@dsys.ru>
2020-12-27 12:02:24 +0300

    task done with function

--- HW15_4.cpp
+++ HW15_4.cpp
@@ -1,5 +1,30 @@
 #include <iostream>
 
+void PrintNumbers(bool type, int N)
+{
+	if (type)
+	{
+		for (int i = 0; i < N; i++)
+		{
+			if (!(i % 2))
+			{
+				std::cout << i << " ";
+			}
+		}
+	}
+	else
+	{
+		for (int i = 0; i <= N; i++)
+		{
+			if (i % 2)
+			{
+				std::cout << i << " ";
+			}
+		}
+	}
+}
+
+
 int main()
 {
 	int N = 25; // max number for printing
@@ -11,5 +36,12 @@ int main()
 			std::cout << i << " ";
 		}
 	}
+
+	std::cout << std::endl;
+	PrintNumbers(0, N);
+
+	std::cout << std::endl;
+	PrintNumbers(1, N);
+
 	return 0;
 }
